c6bd0a11add6ebde0fa25759d6b4676ab1fadffb
vladimir <vladimir.tryastsyn@gmail.com>
2020-02-18 23:15:15 +0300

    Create commands fill_db select_db clear_db

--- yakimanka/commandapp/admin.py
+++ yakimanka/commandapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Employee, Commissions, Members, Roles, Section
+from .models import Section, Division, Party, Deputy, Roles, Commissions, \
+        Members, Address, ReceptionSchedule, Assistants
 
-# Register your models here.
 
-admin.site.register(Employee)
+admin.site.register(Division)
+admin.site.register(Party)
+admin.site.register(Address)
+admin.site.register(ReceptionSchedule)
+admin.site.register(Assistants)
+admin.site.register(Deputy)
 admin.site.register(Commissions)
 admin.site.register(Members)
 admin.site.register(Roles)
